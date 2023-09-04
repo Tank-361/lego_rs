04c631cc463b189d166efeca714c538910c462e4
Tank-361 <Tank361.py@gmail.com>
2023-09-04 10:24:43 +0800

    Elevator ready for testing

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ use ev3dev_lang_rust::Ev3Result;
 use ev3dev_lang_rust::motors::{LargeMotor, MotorPort};
 use ev3dev_lang_rust::sensors::GyroSensor;
 use std::time::Duration;
+use lib::elevator;
 
 fn main() -> Ev3Result<()> {
     let left_motor = LargeMotor::get(MotorPort::OutB)?;
     let right_motor = LargeMotor::get(MotorPort::OutC)?;
+    let lift_motor = LargeMotor::get(MotorPort::OutA)?;
     let gyro_sensor = GyroSensor::find()?;
 
     let drive: tank_drive::DriveTrain = tank_drive::DriveTrain {
@@ -15,7 +17,13 @@ fn main() -> Ev3Result<()> {
         left: left_motor,
         gyro: gyro_sensor,
     };
+    drive.move_direction(tank_drive::Direction::Forward, Duration::from_secs(20))?;
+    
+    let lift: elevator::Elevator = elevator::Elevator {
+        lift: lift_motor,
+    };
 
-    drive.move_direction(tank_drive::Direction::Forward, Duration::from_secs(2))?;
+    lift.lift(elevator::Direction::Up, Duration::from_secs(2))?;
+
     Ok(())
 }
